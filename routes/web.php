6bb9c8a5a1636144491540499ee7d60a97f62a29
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-04-04 09:21:13 +0700

    update interface UI/UX

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
 });
