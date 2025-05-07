e786bc9fc44f3ba790775b7f59d0526cbcdba08d
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-05-07 14:58:17 +0700

    update 7/5

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,36 +9,40 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
-        $request -> validate([
+    public function register(Request $request)
+    {
+        $request->validate([
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
             'address' => 'required'
         ]);
-        $as = User::create([
-            'name' => $request -> name,
-            'email' => $request -> email,
-            'password' => Hash::make($request -> password),
-            'address' => $request -> address
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'address' => $request->address
         ]);
-        // dd($as);
-        return redirect('/login') -> with('success', 'Đăng ký thành công');
+        return redirect('/login')->with('success', 'Đăng ký thành công');
     }
-    public function login(Request $request) {
-        $credintials = $request -> validate([
+    public function login(Request $request)
+    {
+        $credintials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        if (Auth::attempt($credintials)) {
+        // Kiểm tra nếu người dùng muốn "Nhớ tôi"
+        $remember = $request->filled('remember');
+
+        if (Auth::attempt($credintials, $remember)) {
             $user = Auth::user();
             if ($user->role == 'admin') {
-                return redirect('/admin') -> with('success', 'Đăng nhập thành công');
-            }else{
-                return redirect('/home') -> with('success', 'Đăng nhập thành công');
+                return redirect('/admin')->with('success', 'Đăng nhập thành công');
+            } else {
+                return redirect('/home')->with('success', 'Đăng nhập thành công');
             }
-        }else{
-            return redirect('/login') -> with('error', 'Sai tên email hoặc mật khẩu chưa đúng');
+        } else {
+            return redirect('/login')->with('error', 'Sai tên email hoặc mật khẩu chưa đúng');
         }
     }
 }
