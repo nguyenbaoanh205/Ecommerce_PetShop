7da8f99ac1cd45d46b7edd7e9d076e09da9928fa
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-09-10 16:02:06 +0700

    update

--- resources/views/admin/partials/alert.blade.php
+++ resources/views/admin/partials/alert.blade.php
@@ -13,3 +13,40 @@
         $('.alert').fadeOut('slow');
     }, 3000);
 </script>
+
+{{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@if (session('success'))
+    @if ($isAdmin ?? false)
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @else
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: "{{ session('success') }}",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        </script>
+    @endif
+@endif
+
+@if (session('error'))
+    @if ($isAdmin ?? false)
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @else
+        <script>
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: "{{ session('error') }}",
+                timer: 2000,
+                showConfirmButton: false
+            });
+        </script>
+    @endif
+@endif --}}
